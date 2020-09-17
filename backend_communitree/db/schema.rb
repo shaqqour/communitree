c9a7528c5212bc92e487b93bf1a9ac47f1d73ef4
Shaqqour <mshaqqourm@gmail.com>
2020-09-17 15:42:40 -0700

    Runs rails db:migrate

--- backend_communitree/db/schema.rb
+++ backend_communitree/db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_11_215222) do
+ActiveRecord::Schema.define(version: 2020_09_17_223444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: :cascade do |t|
+    t.string "zip_code"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.string "latitude"
+    t.string "longitude"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
