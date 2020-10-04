982f32450b53ee5ecaf40a4d68d457e3de57dd64
Shaqqour <mshaqqourm@gmail.com>
2020-10-04 11:10:45 -0700

    Migrates the database changes

--- backend_communitree/db/schema.rb
+++ backend_communitree/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_223444) do
+ActiveRecord::Schema.define(version: 2020_10_04_180456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2020_09_17_223444) do
     t.string "country"
     t.string "latitude"
     t.string "longitude"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
