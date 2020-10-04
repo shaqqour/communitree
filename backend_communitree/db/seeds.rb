42b359be65410f71813fe5f23227d46dc9a4bb67
Shaqqour <mshaqqourm@gmail.com>
2020-10-04 08:18:44 -0700

    Seeds some data in the seeds.rb file and run rails db:seed

--- backend_communitree/db/seeds.rb
+++ backend_communitree/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+    User.create(username: "mikelee", first_name: "Mike", last_name: "Lee", birth_date: Date.new(1990, 1, 1), age: Time.now.year - 1990, phone_number: "0000000000", email: "mikelee@email.com", password: "000000")
+    User.create(username: "jakefee", first_name: "Jake", last_name: "Fee", birth_date: Date.new(1980, 1, 1), age: Time.now.year - 1980, phone_number: "1111111111", email: "jakefee@email.com", password: "111111")
+
+    Location.create(zip_code: "00000", city: "Man", state: "CA", country: "U.S", latitude: "0", longitude: "0")
+    Location.create(zip_code: "11111", city: "Fan", state: "CA", country: "U.S", latitude: "1", longitude: "1")
