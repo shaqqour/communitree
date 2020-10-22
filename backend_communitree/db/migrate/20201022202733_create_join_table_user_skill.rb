94fc7e414dad0a3ada63a5acf1662c230e45d760
Shaqqour <mshaqqourm@gmail.com>
2020-10-22 13:44:51 -0700

    Rolls back the database changes regarding the userskill join table and destroy the migration

--- backend_communitree/db/migrate/20201022202733_create_join_table_user_skill.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableUserSkill < ActiveRecord::Migration[6.0]
-  def change
-    create_join_table :users, :skills do |t|
-      # t.index [:user_id, :skill_id]
-      # t.index [:skill_id, :user_id]
-    end
-  end
-end
