53d5361351cd2d44f1b367b2633db95435964284
Shaqqour <mshaqqourm@gmail.com>
2020-10-22 13:35:41 -0700

    Creates a join table to handle the many to many relationship between user and skill

--- backend_communitree/db/schema.rb
+++ backend_communitree/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_18_212844) do
+ActiveRecord::Schema.define(version: 2020_10_22_202733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,11 @@ ActiveRecord::Schema.define(version: 2020_10_18_212844) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "skills_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "skill_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
