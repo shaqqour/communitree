579c4017de177c4f7510eccda238ac2b090876f8
Shaqqour <mshaqqourm@gmail.com>
2020-10-27 12:13:27 -0700

    Creates show action in the UsersController

--- backend_communitree/app/controllers/users_controller.rb
+++ backend_communitree/app/controllers/users_controller.rb
@@ -3,4 +3,8 @@ class UsersController < ApplicationController
         users = User.all
         render json: users
     end
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
 end
