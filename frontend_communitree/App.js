ffb38a8313aef017b6e5be726dfb986300c89ab7
Shaqqour <mshaqqourm@gmail.com>
2020-11-08 12:06:52 +0400

    Adds CreateAccountScreen to App.js

--- frontend_communitree/App.js
+++ frontend_communitree/App.js
@@ -1,12 +1,14 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import CreateAccountScreen from './src/screens/CreateAccountScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import LoginScreen from './src/screens/LoginScreen';
 
 const AppNavigator = createStackNavigator(
   {
     Home: HomeScreen,
-    Login: LoginScreen
+    Login: LoginScreen,
+    CreateAccount: CreateAccountScreen
   }
 );
 
