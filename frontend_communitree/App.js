219db87af916db9517994e8c5c991e301cbed141
Shaqqour <mshaqqourm@gmail.com>
2020-11-08 12:05:53 +0400

    Adds LoginScreen to App.js

--- frontend_communitree/App.js
+++ frontend_communitree/App.js
@@ -1,10 +1,12 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from './src/screens/HomeScreen';
+import LoginScreen from './src/screens/LoginScreen';
 
 const AppNavigator = createStackNavigator(
   {
-    Home: HomeScreen
+    Home: HomeScreen,
+    Login: LoginScreen
   }
 );
 
