945830d3513d6b0243cac05b1e100345bf121461
Shaqqour <mshaqqourm@gmail.com>
2020-11-21 15:42:26 +0200

    Adds a Logout button to the Homescreen that navigates to the Login screen onPress

--- frontend_communitree/src/screens/HomeScreen.js
+++ frontend_communitree/src/screens/HomeScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>HomeScreen</Text>
+            <Button title="Log out" onPress={() => navigation.navigate('Login')} />
         </View>
     );
 }
