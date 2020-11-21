5a8fba3f154e4dbec4998d2371f9b60c1913e281
Shaqqour <mshaqqourm@gmail.com>
2020-11-21 16:48:52 +0200

    Adds a Create account button to the LoginScreen that navigates to the create account  screen onPress

--- frontend_communitree/src/screens/LoginScreen.js
+++ frontend_communitree/src/screens/LoginScreen.js
@@ -6,6 +6,7 @@ const LoginScreen = ({ navigation }) => {
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>LoginScreen</Text>
             <Button title="Log in" onPress={() => navigation.navigate('Home')} />
+            <Button title="Create account" onPress={() => navigation.navigate('CreateAccount')} />
         </View>
     );
 }
