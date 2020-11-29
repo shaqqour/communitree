9f1e585cb44da5b6b6cffde8fef70dc8fd60d4f0
Shaqqour <mshaqqourm@gmail.com>
2020-11-29 16:35:57 +0200

    Removes foreign key refrences from requested and offered user in the migration because there is no table with those names to be able to create the service with two foreign keys from the user table

--- backend_communitree/db/migrate/20201129141616_create_services.rb
+++ backend_communitree/db/migrate/20201129141616_create_services.rb
@@ -4,10 +4,12 @@ class CreateServices < ActiveRecord::Migration[6.0]
       t.string :title
       t.string :description
       t.references :category, null: false, foreign_key: true
-      t.references :requested_user, null: false, foreign_key: true
-      t.references :offered_user, null: false, foreign_key: true
+      t.references :requested_user, null: false
+      t.references :offered_user, null: false
 
       t.timestamps
     end
+    add_foreign_key :services, :users, column: :requested_user_id
+    add_foreign_key :services, :users, column: :offered_user_id
   end
 end
