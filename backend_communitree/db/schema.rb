a553f68d50a10c64b7f78601f87c5651a4f48af3
Shaqqour <mshaqqourm@gmail.com>
2020-11-29 16:40:30 +0200

    Migrates Service table changes by running rails db:migrates

--- backend_communitree/db/schema.rb
+++ backend_communitree/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_29_133113) do
+ActiveRecord::Schema.define(version: 2020_11_29_141616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,19 @@ ActiveRecord::Schema.define(version: 2020_11_29_133113) do
     t.string "longitude"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.bigint "category_id", null: false
+    t.bigint "requested_user_id", null: false
+    t.bigint "offered_user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_services_on_category_id"
+    t.index ["offered_user_id"], name: "index_services_on_offered_user_id"
+    t.index ["requested_user_id"], name: "index_services_on_requested_user_id"
   end
 
   create_table "skills", force: :cascade do |t|
@@ -64,4 +77,7 @@ ActiveRecord::Schema.define(version: 2020_11_29_133113) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "services", "categories"
+  add_foreign_key "services", "users", column: "offered_user_id"
+  add_foreign_key "services", "users", column: "requested_user_id"
 end
