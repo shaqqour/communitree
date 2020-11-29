e47dcce33886bd689b9d52ab109cbd4bffb535d8
Shaqqour <mshaqqourm@gmail.com>
2020-11-29 15:40:39 +0200

    Migrates db changes to include category table by running rails db:migrate

--- backend_communitree/db/schema.rb
+++ backend_communitree/db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_23_012834) do
+ActiveRecord::Schema.define(version: 2020_11_29_133113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "locations", force: :cascade do |t|
     t.string "zip_code"
