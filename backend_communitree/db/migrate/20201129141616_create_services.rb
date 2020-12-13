5715cc1345d7a51eb2495e748ff229c5f044701d
Shaqqour <mshaqqourm@gmail.com>
2020-12-13 02:04:00 -0800

    Replaces the two add_foreign_key lines in the migration file with foreign_key: { to_table: :users } to each of the requested_user and offered_user

--- backend_communitree/db/migrate/20201129141616_create_services.rb
+++ backend_communitree/db/migrate/20201129141616_create_services.rb
@@ -4,12 +4,10 @@ class CreateServices < ActiveRecord::Migration[6.0]
       t.string :title
       t.string :description
       t.references :category, null: false, foreign_key: true
-      t.references :requested_user, null: false
-      t.references :offered_user, null: false
+      t.references :requested_user, null: false, foreign_key: { to_table: :users }
+      t.references :offered_user, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
-    add_foreign_key :services, :users, column: :requested_user_id
-    add_foreign_key :services, :users, column: :offered_user_id
   end
 end
