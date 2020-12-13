4510bf7bca682d66588cf1f3a285ec1bba17aedf
Shaqqour <mshaqqourm@gmail.com>
2020-12-13 00:37:27 -0800

    Fixes belongs_to relationship for the Service model to specify the class name for each of the requested and offered user

--- backend_communitree/app/models/service.rb
+++ backend_communitree/app/models/service.rb
@@ -1,5 +1,5 @@
 class Service < ApplicationRecord
   belongs_to :category
-  belongs_to :requested_user
-  belongs_to :offered_user
+  belongs_to :requested_user, class_name: 'User'
+  belongs_to :offered_user, class_name: 'User'
 end
