f67fb00cf1e42ce591d8e3b235afc04c3fe1a249
Shaqqour <mshaqqourm@gmail.com>
2021-03-11 18:57:50 -0800

    Declares firstName and lastName using react hooks

--- frontend_communitree/src/screens/CreateAccountScreen.js
+++ frontend_communitree/src/screens/CreateAccountScreen.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button, TextInput } from 'react-native';
 
 const CreateAccountScreen = ({ navigation }) => {
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>CreateAccountScreen</Text>
+            <Text>Sign Up</Text>
+            <TextInput placeholder="First Name" onChange={firs}/>
+            <TextInput placeholder="Last Name" />
+            
             <Button title="Log in" onPress={() => navigation.navigate('Login')} />
         </View>
     );
