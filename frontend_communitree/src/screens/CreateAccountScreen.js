d751ddf923f4b853e4615244fdfddb22938877b7
Shaqqour <mshaqqourm@gmail.com>
2021-03-30 13:06:51 -0700

    Adds firstName and lastName to sign-up/create account page

--- frontend_communitree/src/screens/CreateAccountScreen.js
+++ frontend_communitree/src/screens/CreateAccountScreen.js
@@ -9,8 +9,10 @@ const CreateAccountScreen = ({ navigation }) => {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Sign Up</Text>
-            <TextInput placeholder="First Name" onChange={firs}/>
-            <TextInput placeholder="Last Name" />
+            <TextInput placeholder="First Name" onChangeText={firstName => setFirstName(firstName)}/>
+            <Text>{firstName}</Text>
+            <TextInput placeholder="Last Name" onChangeText={lastName => setLastName(lastName)}/>
+            <Text>{lastName}</Text>
             
             <Button title="Log in" onPress={() => navigation.navigate('Login')} />
         </View>
