800743d489d13abc64a601cbe171dcc38e8c67c9
Shaqqour <mshaqqourm@gmail.com>
2021-03-30 18:28:12 -0700

    Fixes users table for easy testing

--- backend_communitree/db/migrate/20200911215222_devise_create_users.rb
+++ backend_communitree/db/migrate/20200911215222_devise_create_users.rb
@@ -4,12 +4,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :username
+      # t.string :username
       t.string :first_name
       t.string :last_name
-      t.date :birth_date
-      t.integer :age
-      t.string :phone_number
+      # t.date :birth_date
+      # t.integer :age
+      # t.string :phone_number
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
--- backend_communitree/db/schema.rb
+++ backend_communitree/db/schema.rb
@@ -59,12 +59,8 @@ ActiveRecord::Schema.define(version: 2020_11_29_141616) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
     t.string "first_name"
     t.string "last_name"
-    t.date "birth_date"
-    t.integer "age"
-    t.string "phone_number"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
